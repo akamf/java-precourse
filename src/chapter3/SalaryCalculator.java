513e698f906affe85e452b7c0595a77d0b1b1fba
Andreas Kamf <andreaskamf@gmail.com>
2025-02-17 13:16:44 +0100

    initial chapter3 setup

---
+++ src/chapter3/SalaryCalculator.java
@@ -0,0 +1,15 @@
+package chapter3;
+
+import java.util.Scanner;
+
+public class SalaryCalculator {
+    public static final int WEEKLY_SALARY = 1000;
+    public static final int SALES_BONUS = 250;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
+    }
+
+}
