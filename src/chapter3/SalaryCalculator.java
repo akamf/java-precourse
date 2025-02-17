acf200c96f23118966e05969365a4d5b982e25a6
Andreas Kamf <andreaskamf@gmail.com>
2025-02-17 16:38:44 +0100

    SalrayCalculator initialization with bonus system

--- src/chapter3/SalaryCalculator.java
+++ src/chapter3/SalaryCalculator.java
@@ -3,12 +3,23 @@ package chapter3;
 import java.util.Scanner;
 
 public class SalaryCalculator {
-    public static final int WEEKLY_SALARY = 1000;
+    public static final int BASE_SALARY = 1000;
     public static final int SALES_BONUS = 250;
+    public static final int BONUS_THRESHOLD = 10;
 
     public static void main(String[] args) {
+        int totalSales;
+        int salary = BASE_SALARY;
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("How many sales did the employee make this week?\n>> ");
+        totalSales = scanner.nextInt();
+
+        if (totalSales >= BONUS_THRESHOLD) {
+            salary += SALES_BONUS;
+        }
+
+        System.out.printf("The employee's pay is $%d", salary);
         scanner.close();
     }
 
