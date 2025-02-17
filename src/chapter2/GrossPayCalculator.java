ec1fa273c1efab78b2c89a1f22ff40153cb2bf5d
Andreas Kamf <andreaskamf@gmail.com>
2025-02-17 12:29:40 +0100

    finalized GrossPayCalculator for chapter2

--- src/chapter2/GrossPayCalculator.java
+++ src/chapter2/GrossPayCalculator.java
@@ -1,7 +1,22 @@
 package chapter2;
 
-public class GrossPayCalculator {
-    public static void main(String arg[]) {
+import java.util.Scanner;
 
+public class GrossPayCalculator {
+
+    public static void main(String arg[]) {
+        System.out.print("Enter the number of hours the employee worked:\n>> ");
+        Scanner scanner = new Scanner(System.in);
+        int hoursWorked = scanner.nextInt();
+
+        System.out.print("Enter the employees pay rate:\n>> ");
+        double payRate = scanner.nextDouble();
+
+        double grossPay = hoursWorked * payRate;
+
+        System.out.print("The employee's gross pay is: $" + grossPay);
+
+        scanner.close();
     }
+
 }
