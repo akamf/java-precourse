b3f6f47391feca78f3291fff4c90da4434814de4
Andreas Kamf <andreaskamf@gmail.com>
2025-02-17 16:43:34 +0100

    Initial structure for Quotacalculator

---
+++ src/chapter3/QuotaCalculator.java
@@ -0,0 +1,15 @@
+package chapter3;
+
+import java.util.Scanner;
+
+public class QuotaCalculator {
+    public static final int QUOTA_THRESHOLD = 10;
+
+    public static void main(String[] args) {
+        int numSales;
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
+    }
+
+}
