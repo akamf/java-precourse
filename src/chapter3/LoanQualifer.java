f0ce3e24b4eb0bd57947f9d6473bad3be6054483
Andreas Kamf <andreaskamf@gmail.com>
2025-02-17 22:31:15 +0100

    setup LoanQualifer

---
+++ src/chapter3/LoanQualifer.java
@@ -0,0 +1,14 @@
+package chapter3;
+
+import java.util.Scanner;
+
+public class LoanQualifer {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
+    }
+
+}
