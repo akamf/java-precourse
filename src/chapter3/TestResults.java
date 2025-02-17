0e018145803d4159e0ad23b5808358596b9c9463
Andreas Kamf <andreaskamf@gmail.com>
2025-02-17 22:52:19 +0100

    finalized TestResult class

--- src/chapter3/TestResults.java
+++ src/chapter3/TestResults.java
@@ -1,9 +1,30 @@
 package chapter3;
 
+import java.util.Scanner;
+
 public class TestResults {
 
     public static void main(String[] args) {
+        double score;
+        char grade;
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter your test score:\n>> ");
+        score = scanner.nextDouble();
+
+        if (score < 60)
+            grade = 'F';
+        else if (score < 70)
+            grade = 'D';
+        else if (score < 80)
+            grade = 'C';
+        else if (score < 90)
+            grade = 'B';
+        else
+            grade = 'A';
+
+        System.out.printf("Your grade is %s", grade);
+        scanner.close();
     }
-    
+
 }
