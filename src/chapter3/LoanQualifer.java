6ff5a6c96db649a3c4d77fde8b1bcc800b0ea9db
Andreas Kamf <andreaskamf@gmail.com>
2025-02-17 22:45:50 +0100

    finished LoanQualifier

--- src/chapter3/LoanQualifer.java
+++ src/chapter3/LoanQualifer.java
@@ -7,7 +7,27 @@ public class LoanQualifer {
     public static final int REQUIRED_YEARS_EMPLOYED = 2;
 
     public static void main(String[] args) {
+        double salary, yearsEmployed;
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your salary:\n>> ");
+        salary = scanner.nextDouble();
+
+        System.out.print("Enter the number of years with your current employer:\n>> ");
+        yearsEmployed = scanner.nextDouble();
+
+        if (salary >= REQUIRED_SALARY && yearsEmployed >= REQUIRED_YEARS_EMPLOYED) {
+            System.out.println("Congratulations! You qualify for a loan.");
+        }
+        else {
+            System.out.println("Sorry! You don't qualify for a loan.\nREASON(S)");
+            if (salary < REQUIRED_SALARY) {
+                System.out.printf("* You have to earn at least $%d.\n", REQUIRED_SALARY);
+            }
+            if (yearsEmployed < REQUIRED_YEARS_EMPLOYED) {
+                System.out.printf("* You have to work at least %d years at the same company.\n", REQUIRED_YEARS_EMPLOYED);
+            }
+        }
 
         scanner.close();
     }
