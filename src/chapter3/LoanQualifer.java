f670a03eb411cb6f6c3f39418316ecb1d3e9c4ab
Andreas Kamf <andreaskamf@gmail.com>
2025-02-17 22:32:12 +0100

    add constant values

--- src/chapter3/LoanQualifer.java
+++ src/chapter3/LoanQualifer.java
@@ -3,7 +3,8 @@ package chapter3;
 import java.util.Scanner;
 
 public class LoanQualifer {
-
+    public static final int REQUIRED_SALARY = 30_000;
+    public static final int REQUIRED_YEARS_EMPLOYED = 2;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
