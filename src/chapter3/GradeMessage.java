799ba272daea13e8029dd7fc73d2aa78c7d6055c
Andreas Kamf <andreaskamf@gmail.com>
2025-02-17 22:54:31 +0100

    Setup GradeMessage

---
+++ src/chapter3/GradeMessage.java
@@ -0,0 +1,13 @@
+package chapter3;
+
+import java.util.Scanner;
+
+public class GradeMessage {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
+    }
+
+}
