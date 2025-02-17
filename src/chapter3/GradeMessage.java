86ed2cf3a8ac5d2c6f9939f0f789e10c1ce3db14
Andreas Kamf <andreaskamf@gmail.com>
2025-02-17 23:01:00 +0100

    finalized GradeMessage class

--- src/chapter3/GradeMessage.java
+++ src/chapter3/GradeMessage.java
@@ -5,7 +5,34 @@ import java.util.Scanner;
 public class GradeMessage {
 
     public static void main(String[] args) {
+        String grade, message;
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your letter grade:\n>> ");
+        grade = scanner.next();
+
+        switch (grade.toUpperCase()) {
+            case "A":
+                message = "Excellent job!";
+                break;
+            case "B":
+                message = "Great job!";
+                break;
+            case "C":
+                message = "Good job!";
+                break;
+            case "D":
+                message = "You need to work a little harder!";
+                break;
+            case "F":
+                message = "Uh oh!";
+                break;
+            default:
+                message = "Error! Invalid grade";
+                break;
+        }
+
+        System.out.println(message);
 
         scanner.close();
     }
