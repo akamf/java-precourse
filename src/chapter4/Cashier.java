96f0976c212e009f306bf3c656393ca62d3f2360
Andreas Kamf <andreaskamf@gmail.com>
2025-02-18 12:53:32 +0100

    finished Cashier class

--- src/chapter4/Cashier.java
+++ src/chapter4/Cashier.java
@@ -9,6 +9,18 @@ public class Cashier {
         int quantity = 0;
         double total = 0.0;
 
+        System.out.printf("Enter the number of items you would like to scan:%n>> ");
+        quantity = SCANNER.nextInt();
+
+        for (int i = 0; i < quantity; i++) {
+            System.out.printf("Enter the cost of the item:%n>> ");
+            double price = SCANNER.nextDouble();
+
+            total += price;
+        }
+
+        System.out.printf("The total sum of the %d items is $%.2f.", quantity, total);
+
         SCANNER.close();
     }
 }
