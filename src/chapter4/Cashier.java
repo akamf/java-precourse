f668b95ec3ce5d5e6297a078897ea92612e1d9ab
Andreas Kamf <andreaskamf@gmail.com>
2025-02-18 12:47:23 +0100

    initial setup for Cashier

---
+++ src/chapter4/Cashier.java
@@ -0,0 +1,14 @@
+package chapter4;
+
+import java.util.Scanner;
+
+public class Cashier {
+    public static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int quantity = 0;
+        double total = 0.0;
+
+        SCANNER.close();
+    }
+}
