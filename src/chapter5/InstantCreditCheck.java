5ef3a84a90022796e4c1ad2ca09c63a94294e730
Andreas Kamf <andreaskamf@gmail.com>
2025-02-18 15:13:35 +0100

    inital InstantCreditCheck setup

---
+++ src/chapter5/InstantCreditCheck.java
@@ -0,0 +1,13 @@
+package chapter5;
+
+import java.util.Scanner;
+
+public class InstantCreditCheck {
+    public static Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+        SCANNER.close();
+    }
+}
