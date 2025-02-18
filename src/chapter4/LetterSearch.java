9ac7313106c4a38715f0296dad590b6d6ba92b6b
Andreas Kamf <andreaskamf@gmail.com>
2025-02-18 12:55:37 +0100

    initial setup for LetterSearch

---
+++ src/chapter4/LetterSearch.java
@@ -0,0 +1,13 @@
+package chapter4;
+
+import java.util.Scanner;
+
+public class LetterSearch {
+    public  static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        SCANNER.close();
+    }
+
+}
