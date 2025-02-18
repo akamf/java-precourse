4713ef71920dab08cf25b43dd9a2d504926e14a0
Andreas Kamf <andreaskamf@gmail.com>
2025-02-18 15:09:16 +0100

    started chapter5 and initial setup for Greetings

---
+++ src/chapter5/Greetings.java
@@ -0,0 +1,15 @@
+package chapter5;
+
+import java.util.Scanner;
+
+public class Greetings {
+    public static Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String name;
+
+
+        SCANNER.close();
+    }
+
+}
