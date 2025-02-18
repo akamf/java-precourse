559ed95023cfbb7d20dd1a6d8878e40ed80bb393
Andreas Kamf <andreaskamf@gmail.com>
2025-02-18 12:21:14 +0100

    Initial structure AddNumbers

---
+++ src/chapter4/AddNumbers.java
@@ -0,0 +1,16 @@
+package chapter4;
+
+import java.util.Scanner;
+
+public class AddNumbers {
+    public static Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        boolean runAgain = false;
+
+        do {
+
+        } while (runAgain);
+    }
+
+}
