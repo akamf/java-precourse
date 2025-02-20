0636dbad200ed59f9f4b4cdea9d392a608162abe
Andreas Kamf <andreaskamf@gmail.com>
2025-02-20 12:33:12 +0100

    added mian for chapter 7

---
+++ src/chapter7/ChapterSevenMain.java
@@ -0,0 +1,8 @@
+package chapter7;
+
+public class ChapterSevenMain {
+    public static void main(String[] args) {
+        LotteryTicket ticket = new LotteryTicket();
+        ticket.printNumbers();
+    }
+}
