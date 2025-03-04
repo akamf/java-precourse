ee03f22b796c9a7005b0e87ca790cfc116eec613
Andreas Kamf <andreaskamf@gmail.com>
2025-03-04 11:23:14 +0100

    TextProcessor add spaces uppercase

--- src/chapter8/TextProcessor.java
+++ src/chapter8/TextProcessor.java
@@ -5,6 +5,7 @@ public class TextProcessor {
         String exampleText = "Andreas Kamf SALT Java Fullstack Program Spring 2025";
         countWords(exampleText);
         reverseString(exampleText);
+        addSpaces("AndreasKamfSaltJavaFullstackProgramSpring 2025");
     }
 
     public static void countWords(String text) {
@@ -21,5 +22,19 @@ public class TextProcessor {
     public static void reverseString(String text) {
         for (int i = text.length() - 1; i >= 0; i--)
             System.out.print(text.charAt(i));
+        System.out.println();
+    }
+
+    public static void addSpaces(String text) {
+        var modifiedText = new StringBuilder(text);
+
+        for (int i = 0; i < modifiedText.length(); i++) {
+            if ((i != 0) && Character.isUpperCase(modifiedText.charAt(i))) {
+                modifiedText.insert(i, " ");
+                i++;
+            }
+        }
+
+        System.out.println(modifiedText);
     }
 }
