f4109e89ed1e255b611037c1b5d741b21341061e
Andreas Kamf <andreaskamf@gmail.com>
2025-03-04 11:55:48 +0100

    added general Main for chapter2 with MadLibsGenerator

---
+++ src/chapter2/ChapterTwoMain.java
@@ -0,0 +1,14 @@
+package chapter2;
+
+public class ChapterTwoMain {
+    public static void main(String[] args) {
+        MadLibsGenerator madLibsGenerator = new MadLibsGenerator();
+
+        System.out.println("\n---DEFAULT GENERATED MAD LIBS---");
+        madLibsGenerator.print();
+
+        madLibsGenerator.create();
+        System.out.println("\n---USER GENERATED MAD LIBS---");
+        madLibsGenerator.print();
+    }
+}
