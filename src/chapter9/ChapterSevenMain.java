09e9cdfc2d890a4e9f9d35bc0ee9adfc837a3b5a
Andreas Kamf <andreaskamf@gmail.com>
2025-03-06 13:39:09 +0100

    chain of inheritance Person -> Woman -> Mom

--- src/chapter9/ChapterSevenMain.java
+++ src/chapter9/ChapterSevenMain.java
@@ -2,15 +2,19 @@ package chapter9;
 
 public class ChapterSevenMain {
     public static void main(String[] args) {
-        //Person person1 = new Person();
-        //Person person2 = new Person("Andreas");
-        //Employee employee = new Employee();
+//        Person person1 = new Person();
+//        Person person2 = new Person("Andreas");
+//        Employee employee = new Employee();
 
-        Rectangle rectangle = new Rectangle();
-        rectangle.print();
+//        Rectangle rectangle = new Rectangle();
+//        rectangle.print();
+//
+//        Square square = new Square();
+//        square.print();
+//        square.print("Square");
 
-        Square square = new Square();
-        square.print();
-        square.print("Square");
+        Mother mom = new Mother();
+        mom.setName("Mega-Mom");
+        System.out.println(mom.getName() + "'s gender is " + mom.getGender());
     }
 }
