b1e4b557e70ac36eb9385d05e30549bf79a15a24
Andreas Kamf <andreaskamf@gmail.com>
2025-03-12 10:39:33 +0100

    fix

--- src/chapter3/TestResults.java
+++ src/chapter3/TestResults.java
@@ -7,7 +7,7 @@ public class TestResults {
         Scanner scanner = new Scanner(System.in);
 
         System.out.printf("Enter your test score:%n>> ");
-        
+
         double score = scanner.nextDouble();
         char grade = getGrade(score);
         String message = getGradeMessage(grade);
